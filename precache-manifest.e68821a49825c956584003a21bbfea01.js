37746ae9d487612b7a2922c51828d3afaa794385
ast_yzakharevych <y.zakharevych@astoundcommerce.com>
2019-06-26 17:55:02 +0300

    Updates

---
+++ precache-manifest.e68821a49825c956584003a21bbfea01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa801cb73c50e08a1d7cde91ff6b96dd",
+    "url": "/react-airport/index.html"
+  },
+  {
+    "revision": "2d5d3a60b010223674cf",
+    "url": "/react-airport/static/css/main.607269bb.chunk.css"
+  },
+  {
+    "revision": "db0c0faed99554c7cb65",
+    "url": "/react-airport/static/js/2.fde99a95.chunk.js"
+  },
+  {
+    "revision": "2d5d3a60b010223674cf",
+    "url": "/react-airport/static/js/main.845b41c7.chunk.js"
+  },
+  {
+    "revision": "e0bd1c1fa8216a051281",
+    "url": "/react-airport/static/js/runtime~main.0a4d1a4b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ce3a800a70812201f6620dd506f739ca.js"
+  "/react-airport/precache-manifest.e68821a49825c956584003a21bbfea01.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-airport/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
