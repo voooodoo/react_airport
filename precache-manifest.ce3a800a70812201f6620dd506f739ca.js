2bee976ff071a230ed92203884ec193fb8a0fc20
ast_yzakharevych <y.zakharevych@astoundcommerce.com>
2019-06-26 17:53:08 +0300

    Updates

---
+++ precache-manifest.ce3a800a70812201f6620dd506f739ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11fbe77c71e96b519a3e89db29fe7d26",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2d5d3a60b010223674cf",
+    "url": "./static/css/main.607269bb.chunk.css"
+  },
+  {
+    "revision": "db0c0faed99554c7cb65",
+    "url": "./static/js/2.fde99a95.chunk.js"
+  },
+  {
+    "revision": "2d5d3a60b010223674cf",
+    "url": "./static/js/main.845b41c7.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
